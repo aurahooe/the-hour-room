10c7137b246cdd85eea7242613774eca765616e1
aurahooe <auvodka@reborn.com>
2026-09-23 05:19:19 +0100

    Add root layout

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "The Hour Room",
+  description: "A living editorial room. Public writing is featured every hour."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,400;0,9..144,500;1,9..144,400&family=IBM+Plex+Mono:wght@400;500&family=Newsreader:ital,opsz,wght@0,6..72,400;0,6..72,500;1,6..72,400&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
